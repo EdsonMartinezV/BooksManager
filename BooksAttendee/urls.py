901d98d410dbbcc1cfdf55c0e9f95c958ad1c628
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-05-31 21:11:08 -0500

    CRUD check

--- BooksAttendee/urls.py
+++ BooksAttendee/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'BooksAttendee'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:book_id>/', views.detail, name='detail'),
+    path('<int:book_id>/update/', views.update, name='update'),
+    path('<int:book_id>/delete/', views.delete, name='delete'),
 ]
--- BooksAttendee/views.py
+++ BooksAttendee/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from BooksAttendee.models import Book
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the books index.")
+    books_list = Book.objects.order_by('-created_at')
+    context = {'books_list': books_list}
+    return render(request, 'BooksAttendee/index.html', context)
+
+def create(request):
+    if request.method == 'GET':
+        return render(request, 'BooksAttendee/create.html')
+    elif request.method == 'POST':
+        book = Book(title=request.POST['title'], description=request.POST['description'])
+        book.save()
+        return HttpResponseRedirect(reverse('BooksAttendee:index'))
+
+def detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'BooksAttendee/detail.html', {'book': book})
+
+def update(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    book.title = request.POST['title']
+    book.description = request.POST['description']
+    book.save()
+    return HttpResponseRedirect(reverse('BooksAttendee:detail', args=(book.id,)))
+
+def delete(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return HttpResponseRedirect(reverse('BooksAttendee:index'))
+
